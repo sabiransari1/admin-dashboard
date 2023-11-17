eb96ec487f7c00707e5042b7c4679dccc4c14726
Sabir Ansari <ansariisabir1@gmail.com>
2023-11-17 01:06:29 -0800

    updates

--- src/components/CategoryChart.jsx
+++ src/components/CategoryChart.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Pie } from "react-chartjs-2";
+import { Doughnut } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 import { Box } from "@chakra-ui/react";
 
@@ -8,7 +8,7 @@ export const CategoryChart = ({ category }) => {
 
   return (
     <Box>
-      <Pie
+      <Doughnut
         data={{
           labels: [
             "Suits",
